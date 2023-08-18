98cab305e48d135b35f1c89759cad3482eacbcc4
SimY4 <alex.simkin@outlook.com>
2023-08-18 14:03:10 +1000

    Simplify isDigit.

--- core/src/main/java/com/github/simy4/coregex/core/CoregexParser.java
+++ core/src/main/java/com/github/simy4/coregex/core/CoregexParser.java
@@ -567,21 +567,7 @@ public final class CoregexParser {
    * }</pre>
    */
   private boolean isDigit(int ch) {
-    switch (ch) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        return true;
-      default:
-        return false;
-    }
+    return ((ch - '0') | ('9' - ch)) >= 0;
   }
 
   private boolean isWhitespace(int ch) {
